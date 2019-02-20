1a1439a45e9d2ba9e6305c601209a1eb3f6b75bc
Aleksandr <alexbutol@yandex.ru>
2019-02-20 18:10:35 +0300

    create lab_06
    start lab_06

---
+++ lab_06/lab_06/Conveyor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_06
+{
+    class Conveyor
+    {
+        public Conveyor()
+        {
+            
+        }
+
+        public bool It_free(bool status) // true - свободен ; false - не свободен
+        {
+            return status;
+        }
+
+        public void GetTask()
+        {
+
+        }
+    }
+}
---
+++ lab_06/lab_06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conveyor third_line = new Conveyor(3, null);
+            Conveyor second_line = new Conveyor(3, third_line);
+            Conveyor first_line = new Conveyor(3, second_line);
+        }
+    }
+}
